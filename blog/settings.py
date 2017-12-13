f31eb744abfdbcfb371bb1bf055f2224bc2d4e69
Matt Rudge <matt.rudge@codeinstitute.net>
2017-12-13 12:45:34 +0000

    Added Heroku and Postgres support for deployment

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-shever73.c9users.io']
+ALLOWED_HOSTS = ['django-blog-shever73.c9users.io', 'blog-test-app.herokuapp.com']
 
 
 # Application definition
